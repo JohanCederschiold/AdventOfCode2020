e9f83a10e3af1720d4273b994a049292802c6696
Johan Cederschiold <johan.cederschiold@stralfors.se>
2020-12-17 11:24:17 +0100

    solution 2 day 15

--- src/main/java/fifteen/Johan2.java
+++ src/main/java/fifteen/Johan2.java
@@ -6,30 +6,46 @@ import java.util.Map;
 public class Johan2 {
 
     private static long [] sequence = {2,1,10,11,0,6};
-    private static long [] testSequence = {0,3,6};
-    private static long noRounds = 2020;
+    private static long [] testSequence = {3,2,1};
+    private static long noRounds = 30000000;
     private static Map<Long, Long> previouslySpoken;
+    private static long latestNumber;
 
 
     public static void main(String[] args) {
-        playGame(testSequence);
+
+        previouslySpoken = new HashMap<Long, Long>();
+        playGame(sequence);
     }
 
     private static void playGame (long [] sequence)  {
-        previouslySpoken = new HashMap<Long, Long>();
         instantiateSequence(sequence);
-        long latestNumber = sequence[sequence.length - 1];
         for (long round = sequence.length + 1 ; round <= noRounds ; round ++  ) {
-            if (previouslySpoken.containsKey(latestNumber)) {
-
+            long indexLastSpoken = checkIfPreviouslySpoken(latestNumber);
+            previouslySpoken.put(latestNumber, round - 1);
+            if (indexLastSpoken == -1 ) {
+                latestNumber = 0;
+            } else {
+                latestNumber = round - indexLastSpoken -1 ;
             }
         }
+        System.out.println(latestNumber);
     }
 
     private static void instantiateSequence(long[] sequence) {
-        for (long round = 1 ; round <= sequence.length ; round++ ){
+        for (long round = 1 ; round < sequence.length ; round++ ){
             previouslySpoken.put(sequence[(int) round - 1], round);
         }
+        latestNumber = sequence[sequence.length - 1];
     }
 
+    private static long checkIfPreviouslySpoken (long numberToCheck) {
+        long index;
+        try {
+            index = previouslySpoken.get(numberToCheck);
+        } catch (NullPointerException e) {
+            index = -1;
+        }
+        return index;
+    }
 }
